7b10700c7843356b83591c5718e7657a7e212ca2
Bao <162453602+DT2BR@users.noreply.github.com>
2025-10-13 22:18:06 +0700

    made featured game prettier

--- game demo/Clappy-Birb/script/Player.js
+++ game demo/Clappy-Birb/script/Player.js
@@ -7,7 +7,7 @@ export class Player{
         this.y = 100;
         this.image = document.getElementById("Player");
         this.velocityY = 0;
-        this.gravity = 0.01;
+        this.gravity = 0.05;
         this.jumpstrength = -1;
     }
 
